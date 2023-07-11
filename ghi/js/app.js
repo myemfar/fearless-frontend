37bc8baca3ddeffdc55bb7bbaa68a4ef8b59bd32
Bryan Fischer <myemfar@gmail.com>
2023-07-11 14:20:54 -0600

    cards in columns and date

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,3 +1,18 @@
+function createCard(name, description, pictureUrl, convertedDate) {
+    return `
+      <div class="card shadow p-3 mb-5 bg-white rounded">
+        <img src="${pictureUrl}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p class="card-text">${description}</p>
+        </div>
+        <div class="card-footer text-muted">
+        ${convertedDate}
+        </div>
+      </div>
+    `;
+  }
+
 window.addEventListener('DOMContentLoaded', async () => {
 const url = 'http://localhost:8000/api/conferences/';
 try {
@@ -7,20 +22,42 @@ try {
         // what to do if response is bad
     } else {
         const data = await response.json();
-        const conference = data.conferences[0];
-        const nameTag = document.querySelector('.card-title');
-        nameTag.innerHTML = conference.name;
+        let col = -1
+        for (let conference of data.conferences){
+            const detailUrl = `http://localhost:8000${conference.href}`;
+            const detailResponse = await fetch(detailUrl);
+            if (detailResponse.ok) {
+                col += 1;
+                const details = await detailResponse.json();
+                const title = details.conference.name;
+                const description = details.conference.description;
+                const pictureUrl = details.conference.location.picture_url;
+                const convertedStartDate = details.conference.starts;
+                const convertedEndDate = details.conference.ends;
+                const convertedDate = convertedStartDate.substring(0,10) + ' - ' + convertedEndDate.substring(0, 10)
+                const html = createCard(title, description, pictureUrl, convertedDate);
+                const column = document.querySelectorAll('.col');
+                if (col === 3){
+                    col = 0;
+                };
+                column[col].innerHTML += html;
+                console.log(html);
+            }
+        }
+        // const conference = data.conferences[0];
+        // const nameTag = document.querySelector('.card-title');
+        // nameTag.innerHTML = conference.name;
 
-        const detailUrl = `http://localhost:8000${conference.href}`;
-        const detailResponse = await fetch(detailUrl);
-        if (detailResponse.ok) {
-        const details = await detailResponse.json();
-        const detailTag = document.querySelector('.card-text');
-        detailTag.innerHTML = details.conference.description;
-        console.log(details)
-        const imageTag = document.querySelector('.card-img-top');
-        imageTag.src = details.conference.location.picture_url;
-      }
+        // const detailUrl = `http://localhost:8000${conference.href}`;
+        // const detailResponse = await fetch(detailUrl);
+    //     if (detailResponse.ok) {
+    //     const details = await detailResponse.json();
+    //     const detailTag = document.querySelector('.card-text');
+    //     detailTag.innerHTML = details.conference.description;
+    //     console.log(details)
+    //     const imageTag = document.querySelector('.card-img-top');
+    //     imageTag.src = details.conference.location.picture_url;
+    //   }
     }
 } catch (e) {
     // what to do if an error is raised
